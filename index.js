b313dae997b40882f1b5972208e4f47ce1690cbb
Kevin <kevin.vorsmann@voco.ee>
2024-02-02 15:42:37 +0200

    Displayed an article by slug

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ const path = require('path')
 
 const hbs = require('express-handlebars');
 
-<<<<<<< HEAD
-app.set('views', path.join(__dirname, 'views/layouts'));
-=======
 app.set('views', path.join(__dirname, 'views'));
->>>>>>> 2eeed50 (Displayed all articles)
 app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
 	extname: 'hbs',
@@ -21,37 +17,17 @@ app.engine('hbs', hbs.engine({
 app.use(express.static('public'));
 
 
-<<<<<<< HEAD
-app.get('/', (req, res) => {
-  res.render('main.hbs', { title: 'My Web App' });
-});
-
-
-
-
-const mysql = require('mysql')
-=======
 
 const mysql = require('mysql2')
->>>>>>> 2eeed50 (Displayed all articles)
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
-
-
-<<<<<<< HEAD
-var con = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "MyStrongP@ssw0rd",
-=======
 
 
 var con = mysql.createConnection({
 	host: "localhost",
 	user: "root",
 	password: "qwerty",
->>>>>>> 2eeed50 (Displayed all articles)
 	database: "joga_mysql"
 })
 
@@ -60,8 +36,6 @@ con.connect(function(err) {
 	console.log("Connected to joga_mysql db");
 })
 
-<<<<<<< HEAD
-=======
 app.get('/', (req, res) => {
 	let query = "SELECT * FROM article";
 	let articles = [];
@@ -75,8 +49,19 @@ app.get('/', (req, res) => {
 });
 
 
+app.get('/article/:slug', (req, res) => {
+	let query = `SELECT * FROM article WHERE slug="${req.params.slug}"`
+	let article
+	con.query(query, (err, result) => {
+		if (err) throw err;
+		article = result
+		console.log(article)
+		res.render('article', {
+			article: article
+		})
+	});
+});
 
->>>>>>> 2eeed50 (Displayed all articles)
 app.listen(3000, () => {
 	console.log('App is started at http://localhost:3000');
 });
